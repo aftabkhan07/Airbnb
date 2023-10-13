0d2f82a0c40154d08d42e19e9fa9e5f30ae02c39
aftabkhan07 <khanaftaba1@gmail.com>
2023-10-14 01:33:21 +0530

    first commit

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+import Container from "../container"
+
+const Navbar = () => {
+  return (
+    <div className="fixed w-full bg-white z-10 shaddow-sm">
+      <div className="py-4 border-b-[1px]">
+        <Container>
+          anything
+        </Container>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
